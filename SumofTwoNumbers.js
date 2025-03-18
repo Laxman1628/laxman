40bd37a3793c960a2274581d9711c33557d975ed
Laxman Nemane <laxmann@IISPLPDC.LOCAL>
2025-03-18 16:29:13 +0530

    added code for the sum of two numbers

---
+++ SumofTwoNumbers.js
@@ -0,0 +1,29 @@
+function sumOFTwoNumbers(a, b) {
+  let sum = a + b;
+  return sum;
+}
+
+let result = sumOFTwoNumbers(4, 5);
+
+console.log(result)
+
+
+
+// ------------- 🚀 Pro version   ----------------
+
+
+// function sumOfTwoNumbers(a, b) {
+//     // Input Validation: Ensure both arguments are numbers
+//     if (typeof a !== 'number' || typeof b !== 'number') {
+//       throw new Error('Both arguments must be numbers.');
+//     }
+  
+//     return a + b;
+//   }
+  
+//   try {
+//     const result = sumOfTwoNumbers(4, 5);
+//     console.log('Sum:', result); // Output: Sum: 9
+//   } catch (error) {
+//     console.error(error.message);
+//   }
