b31b4d780235aef0d390351bf0738ad0a4e517c0
Laxman1628 <laxmann@interfaceinfosoft.com>
2025-03-28 17:33:50 +0530

    check string is palindrome or not

---
+++ palindrome.js
@@ -0,0 +1,25 @@
+// function isPalindrome(string){
+//     return string == string.split("").reverse().join("")
+// }
+
+
+// console.log(isPalindrome("namanh"))
+
+function isPalindrome(str) {
+    let j = str.length - 1
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] != str[j]) {
+            return false;
+        }
+        j--;
+    }
+    return true;
+}
+
+let str1 = "racecar";
+let str2 = "nitin";
+let str3 = "Rama";
+
+console.log(isPalindrome(str1));
+console.log(isPalindrome(str2));
+console.log(isPalindrome(str3));
