49cb7371c6a3cf67230dcf08fa398ac259f3ab14
Laxman Nemane <laxmann@IISPLPDC.LOCAL>
2025-04-02 02:34:05 +0530

    added factors of given number

---
+++ factors.js
@@ -0,0 +1,11 @@
+function findAllFactors(n) {
+  let factors = [];
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      factors.push(i);
+    }
+  }
+  return factors;
+}
+
+console.log(findAllFactors(4));
